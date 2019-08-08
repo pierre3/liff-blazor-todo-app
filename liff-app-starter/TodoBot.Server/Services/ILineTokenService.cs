fe9df1177eee7afae0d58be28367d9170927f7d9
pierre3 <pierre3@outlook.com>
2019-08-08 19:38:10 +0900

    delete project in liff-app-starter

--- liff-app-starter/TodoBot.Server/Services/ILineTokenService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace TodoBot.Server.Services
-{
-    public interface ILineTokenService
-    {
-        Task<bool> VerifyTokenAsync(string accessToken);
-    }
-}
