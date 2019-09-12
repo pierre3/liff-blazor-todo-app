e17cfa97df2a8e3d635328230e537804d877abd1
Hirotada Kobayashi <pierre3@outlook.com>
2019-09-12 18:29:33 +0900

    Update for .NET Core 3.0 preview9

--- liff-blazor-todo-app/TodoBot.Client/Startup.cs
+++ liff-blazor-todo-app/TodoBot.Client/Startup.cs
@@ -10,12 +10,12 @@ namespace TodoBot.Client
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<ILiffClient, MockLiffClient>();
-            services.AddSingleton<ITodoClient, MockTodoClient>();
+            //services.AddSingleton<ILiffClient, MockLiffClient>();
+            //services.AddSingleton<ITodoClient, MockTodoClient>();
 
-            //services.AddSingleton<ILiffClient, LiffClient>();
-            //services.AddSingleton<ITodoClient, TodoClient>(provider =>
-            //    new TodoClient(provider.GetService<HttpClient>(), "https://myTodo.azurewebsites.net"));
+            services.AddSingleton<ILiffClient, LiffClient>();
+            services.AddSingleton<ITodoClient, TodoClient>(provider =>
+                new TodoClient(provider.GetService<HttpClient>(), "https://liff-todo.azurewebsites.net"));
         }
 
         public void Configure(IComponentsApplicationBuilder app)
